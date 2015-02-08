1fdbabcd02db2772d3a55d6b733cba6e41e4fc78
ashleylorden <ashley.lorden@lyft.com>
2015-02-08 14:29:59 -0500

    improperly organized js; beginning of work on invite flow

---
+++ hanabi/client/home.js
@@ -0,0 +1,45 @@
+Users = new Mongo.Collection("users");
+var friends;
+var invites;
+
+if (Meteor.isClient) {
+  friends = Meteor.subscribe("friends");
+  invites = Meteor.subscribe("invites");
+
+  Template.body.helpers({
+    friends: function () {
+      return Users.find({userId: {$in: this.friends}});
+    },
+    invites: function () {
+      return Users.find({userId: {$in: this.invites}});
+    },
+    allUsers: function() {
+      return Users.find();
+    }
+  });
+
+  Template.body.events({
+    'click add': function () {
+      // need to implement search
+    }
+  });
+}
+
+Meteor.methods({
+  acceptInvite: function (inviterId) {
+    Users.update(Meteor.userId(), {$set: {friends: inviterId}}); // add to list
+  },
+  rejectInvite: function (inviterId) {
+    Users.update(Meteor.userId(), {$set: {invites: []}}); // remove from list
+  },
+  sendInvite: function (toInviteId) {
+    toInvite = Users.findOne(toInviteId);
+    Users.update(toInvite.id, {$set: {invites: Meteor.userId()}}); // add to list
+  }
+});
+
+if (Meteor.isServer) {
+  Meteor.publish("users", function () {
+    return Users.find();
+  });
+}
